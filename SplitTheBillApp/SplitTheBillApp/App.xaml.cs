90470abfc47184d15f008e01287a74f43793fa3e
vladimiro-fs <vladzyo.gaming@pm.me>
2024-12-21 21:25:46 +0000

    Init project

---
+++ SplitTheBillApp/SplitTheBillApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SplitTheBillApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
